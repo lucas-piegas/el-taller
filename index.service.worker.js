4ae739f6c4281f91a082a5cea1d0ed2efd4af8b6
Lucas <lucaspiegas25@gmail.com>
2026-08-28 12:47:35 -0300

    El dpi de la web, corregido: la letra deja de salir un 37% chica en el telefono

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787930305|1878667';
+const CACHE_VERSION = '1787931926|1907866';
 /** @type {string} */
 const CACHE_PREFIX = 'El Taller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
