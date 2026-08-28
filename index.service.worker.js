3886a5362faecff0bec8286aba5b01a7eb7006cd
Lucas <lucaspiegas25@gmail.com>
2026-08-28 13:27:49 -0300

    El cepillo barre por la diagonal del pulgar, y arranca por la esquina lejana

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787933912|1932999';
+const CACHE_VERSION = '1787934433|1836464';
 /** @type {string} */
 const CACHE_PREFIX = 'El Taller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
