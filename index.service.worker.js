5151a6152037a40c3bac7dc0e6b5d17a67f1faff
Lucas <lucaspiegas25@gmail.com>
2026-08-28 20:33:12 -0300

    Cada moneda dice que compra, la linea ensena a moverse, y el cepillo devuelve el pulgar

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787955157|2312328';
+const CACHE_VERSION = '1787959933|2186184';
 /** @type {string} */
 const CACHE_PREFIX = 'El Taller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
