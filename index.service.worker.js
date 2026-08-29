53d071434071e6823dda1266a89c0357c491e1e6
Lucas <lucaspiegas25@gmail.com>
2026-08-29 14:26:18 -0300

    El mostrador se parte en dos pestañas y el halo señala la que esconde
    El resalte del renglón deja de ser una luz que da la vuelta y pasa a ser el
    mismo halo que late en la puerta de la tienda. Y parado el panel se reparte en
    "Piezas" y "Herramientas": si lo que hay que tocar quedó detrás de la otra
    pestaña, el halo se muda a la pestaña.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788016754|2240879';
+const CACHE_VERSION = '1788024331|1908484';
 /** @type {string} */
 const CACHE_PREFIX = 'El Taller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
