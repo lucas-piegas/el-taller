cf42f13835375540409a5e81ee0d7eca2c48fa78
Lucas <lucaspiegas25@gmail.com>
2026-08-29 11:18:39 -0300

    Una repisa de trofeos no se mide con la vara de un boton

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788011953|2149092';
+const CACHE_VERSION = '1788013063|2091916';
 /** @type {string} */
 const CACHE_PREFIX = 'El Taller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
