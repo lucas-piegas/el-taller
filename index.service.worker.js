2b528294a37daa3383a63223381ee61f557d1d07
Lucas <lucaspiegas25@gmail.com>
2026-08-29 12:06:27 -0300

    La vitrina vuelve entera: el tubo se para al lado de los contadores

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788014796|2210349';
+const CACHE_VERSION = '1788015950|2115359';
 /** @type {string} */
 const CACHE_PREFIX = 'El Taller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
