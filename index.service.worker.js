fae0b065fb28a6310ea038e12d53aa5bec8a2e98
Lucas <lucaspiegas25@gmail.com>
2026-08-29 11:47:13 -0300

    El tubo burbujea en vez de arder, la luz da la vuelta despacio y la flecha se apoya

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788013816|2152942';
+const CACHE_VERSION = '1788014796|2210349';
 /** @type {string} */
 const CACHE_PREFIX = 'El Taller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
