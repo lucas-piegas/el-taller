1b4ed9cba715b83bab329eaf953254a2c7da6f02
Lucas <lucaspiegas25@gmail.com>
2026-08-29 11:00:06 -0300

    El boton del regreso dice lo que da, la rareza se ve, y la ficha de un efecto mide lo que mide el dedo

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787959933|2186184';
+const CACHE_VERSION = '1788011953|2149092';
 /** @type {string} */
 const CACHE_PREFIX = 'El Taller-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
